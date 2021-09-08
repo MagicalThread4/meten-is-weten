e76d35efe3af56a8301d0dd8ed8f8e104aa79c8d
Rob Koster <robkoster2907@gmail.com>
2021-09-08 11:14:52 +0200

    else-statement

--- metenisweten.py
+++ metenisweten.py
@@ -3,6 +3,11 @@ B = str(input("Kies nog een heel getal "))
 
 
 if A > B:
-    print("A is het grootste getal met een waarden met " + A )
+    max = B
+    print("A is het grootste getal met een waarden met " + max )
 elif A < B:
-    print("A is het kleinste getal met een waarden van " + A )
+    min = A
+    print("A is het kleinste getal met een waarden van " + min )
+elif A == B:
+    print("A en B zijn even groot ")
+    
