de5d962d377e43677ebfc358c57c4c456f4287fc
Rob Koster <robkoster2907@gmail.com>
2021-09-08 11:00:34 +0200

    elif-statement

--- metenisweten.py
+++ metenisweten.py
@@ -4,3 +4,5 @@ B = str(input("Kies nog een heel getal "))
 
 if A > B:
     print("A is het grootste getal met een waarden met " + A )
+elif A < B:
+    print("A is het kleinste getal met een waarden van " + A )
